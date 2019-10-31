b2e1b80cb126aaf5a64c75ba22d2de0068c7750b
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-01 00:03:32 +0100

    ADD - add greeting class

---
+++ src/main/java/com/ptl/training/junit5talk/Greeting.java
@@ -0,0 +1,11 @@
+package com.ptl.training.junit5talk;
+
+public class Greeting {
+    public String hello(String name) {
+        return "hello " + name;
+    }
+
+    public String helloWorld() {
+        return "hello world";
+    }
+}
