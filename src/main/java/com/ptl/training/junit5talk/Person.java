7960041c7ffed3a58ceae7a0111eca1afb3a842c
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-03 16:12:05 +0100

    ADD - Person POJO

---
+++ src/main/java/com/ptl/training/junit5talk/Person.java
@@ -0,0 +1,29 @@
+package com.ptl.training.junit5talk;
+
+public class Person {
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+
+    public Person(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void throwsException() {
+        throw new RuntimeException("boom!");
+    }
+}
