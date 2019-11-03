13ac1b64d4c43aa5f1acfac67a10291003ff8d3e
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-03 16:46:21 +0100

    ADD - tag test

---
+++ src/test/java/com/ptl/training/junit5talk/tags/TestInDevelopment.java
@@ -0,0 +1,10 @@
+package com.ptl.training.junit5talk.tags;
+
+import org.junit.jupiter.api.Test;
+
+public class TestInDevelopment {
+    @Test
+    void someTest() {
+        System.out.println("some test in TestInDevelopment");
+    }
+}
---
+++ src/test/java/com/ptl/training/junit5talk/tags/TestInDevelopmentAndProduction.java
@@ -0,0 +1,10 @@
+package com.ptl.training.junit5talk.tags;
+
+import org.junit.jupiter.api.Test;
+
+public class TestInDevelopmentAndProduction {
+    @Test
+    void someTest() {
+        System.out.println("some test in TestInDevelopmentAndProduction");
+    }
+}
