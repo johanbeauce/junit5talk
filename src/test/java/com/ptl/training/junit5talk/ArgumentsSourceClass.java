5d4b91a8441aaae7cfd963f8bef53dad994af787
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-09 22:23:05 +0100

    ADD - parameterized arguments class

---
+++ src/test/java/com/ptl/training/junit5talk/ArgumentsSourceClass.java
@@ -0,0 +1,20 @@
+package com.ptl.training.junit5talk;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class ArgumentsSourceClass implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                arguments("apple", 1, Arrays.asList("a", "b")),
+                arguments("lemon", 2, Arrays.asList("x", "y"))
+        );
+    }
+}
