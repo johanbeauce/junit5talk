5278bf5dd57e0812c1e946cb9251e6ee657a8bee
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-09 15:19:53 +0100

    CHANGE - remove test annotations parameter test

--- src/test/java/com/ptl/training/junit5talk/ParamsTest.java
+++ src/test/java/com/ptl/training/junit5talk/ParamsTest.java
@@ -1,35 +1,27 @@
 package com.ptl.training.junit5talk;
 
-import org.junit.jupiter.api.Test;
-
 public class ParamsTest {
     
-    @Test
     void test_params_value(int id, String name) {
         System.out.println(id + " - " + name);
     }
 
-    @Test
     void test_params_enum_value(int id, String name) {
         System.out.println(id + " - " + name);
     }
 
-    @Test
     void test_params_csv_source(int id, String name) {
         System.out.println(id + " - " + name);
     }
 
-    @Test
     void test_params_csv_file_source(int id, String name) {
         System.out.println(id + " - " + name);
     }
 
-    @Test
     void test_params_method_source(int id, String name) {
         System.out.println(id + " - " + name);
     }
 
-    @Test
     void test_params_arguments_source(int id, String name) {
         System.out.println(id + " - " + name);
     }
