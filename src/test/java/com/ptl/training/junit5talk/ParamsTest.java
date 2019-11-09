3b76f9f22b1cec47b23c5654dcf6653a95a8a96c
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-09 23:19:00 +0100

    CHANGE - params of first methods

--- src/test/java/com/ptl/training/junit5talk/ParamsTest.java
+++ src/test/java/com/ptl/training/junit5talk/ParamsTest.java
@@ -1,13 +1,13 @@
 package com.ptl.training.junit5talk;
 
 public class ParamsTest {
-    
-    void test_params_value(int id, String name) {
-        System.out.println(id + " - " + name);
+
+    void test_params_value(String name) {
+        System.out.println(name);
     }
 
-    void test_params_enum_value(int id, String name) {
-        System.out.println(id + " - " + name);
+    void test_params_enum_value(DAYS day) {
+        System.out.println(day);
     }
 
     void test_params_csv_source(int id, String name) {
