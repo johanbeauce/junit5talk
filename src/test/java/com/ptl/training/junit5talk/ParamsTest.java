e91e0100dae24df8b791cab5764bb42e7070a564
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-09 23:32:52 +0100

    CHANGE - csv source file

--- src/test/java/com/ptl/training/junit5talk/ParamsTest.java
+++ src/test/java/com/ptl/training/junit5talk/ParamsTest.java
@@ -1,5 +1,7 @@
 package com.ptl.training.junit5talk;
 
+import java.util.List;
+
 public class ParamsTest {
 
     void test_params_value(String name) {
@@ -14,15 +16,15 @@ public class ParamsTest {
         System.out.println(id + " - " + name);
     }
 
-    void test_params_csv_file_source(int id, String name) {
-        System.out.println(id + " - " + name);
+    void test_params_csv_file_source(int id, String firstName, String lastName) {
+        System.out.println(id + " - " + firstName + " - " + lastName);
     }
 
     void test_params_method_source(int id, String name) {
         System.out.println(id + " - " + name);
     }
 
-    void test_params_arguments_source(int id, String name) {
+    void test_params_arguments_source(String name, int id, List<String> list) {
         System.out.println(id + " - " + name);
     }
 }
