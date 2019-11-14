88a5e444fe39dd08220f6100ad3f0af099c26b89
Johan BEAUCE <jbeauce@pictet-technologies.com>
2019-11-14 15:24:57 +0100

    ADD - disable test

--- src/test/java/com/ptl/training/junit5talk/IgnoreSomeMethod.java
+++ src/test/java/com/ptl/training/junit5talk/IgnoreSomeMethod.java
@@ -1,7 +1,15 @@
 package com.ptl.training.junit5talk;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class IgnoreSomeMethod {
 
@@ -11,36 +19,43 @@ public class IgnoreSomeMethod {
     }
 
     @Test
+    @Disabled("only run locally")
     void test_which_should_not_run() {
         System.out.println("This test should not run");
     }
 
     @Test
+    @EnabledOnOs(OS.MAC)
     void test_only_on_mac() {
         System.out.println("test_only_on_mac");
     }
 
     @Test
+    @EnabledOnOs(OS.WINDOWS)
     void test_only_on_windows() {
         System.out.println("test_only_on_windows");
     }
 
     @Test
+    @EnabledOnJre(JRE.JAVA_8)
     void test_only_on_jre8() {
         System.out.println("test_only_on_jre8");
     }
 
     @Test
+    @EnabledOnJre(JRE.JAVA_11)
     void test_only_on_jre11() {
         System.out.println("test_only_on_jre11");
     }
 
     @Test
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "jbeauce")
     void test_only_with_jbeauce() {
         System.out.println("test_only_with_jbeauce");
     }
 
     @Test
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "jboss")
     void test_only_with_jboss() {
         System.out.println("test_only_with_jboss");
     }
@@ -48,11 +63,13 @@ public class IgnoreSomeMethod {
 
     @Test
     void test_only_on_dev() {
+        assumeTrue("DEV".equals(System.getProperty("ENV")));
         System.out.println("test_only_on_dev");
     }
 
     @Test
     void test_only_on_prod() {
+        assumeTrue("PROD".equals(System.getProperty("ENV")));
         System.out.println("test_only_on_prod");
     }
 }
